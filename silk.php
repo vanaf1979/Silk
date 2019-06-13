72bd28edf11693db5fae7c24afb1258281e2784d
Stephan <vanaf1979@gmail.com>
2019-06-13 14:55:22 +0200

    initial commit

--- silk.php
+++ silk.php
@@ -15,7 +15,7 @@
  * Author URI:        https://vanaf1979.nl
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain:       sikj
+ * Text Domain:       silk
  * Domain Path:       /languages
  */
 
